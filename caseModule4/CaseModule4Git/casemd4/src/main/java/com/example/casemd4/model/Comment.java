ce2b24fbd1580db199e977e7408e46b4e95241b2
Nguyên Duy <duyiteahp@example.com>
2022-02-12 21:45:26 +0700

    up git lai truoc khi security

---
+++ caseModule4/CaseModule4Git/casemd4/src/main/java/com/example/casemd4/model/Comment.java
@@ -0,0 +1,17 @@
+package com.example.casemd4.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idComment;
+    @ManyToOne
+    private Movie movie;
+
+    private String comment;
+}
